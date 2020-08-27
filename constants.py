aac962c3943dae17275e66edc08791dc20159c00
sustainablelab <sustainablelab@gmail.com>
2020-08-27 00:19:55 -0400

    Add all JSON globals to constants.py

--- constants.py
+++ constants.py
@@ -43,4 +43,28 @@ _constants = dict(zip(_global_names, _global_values))
 GREEN = _constants['LEDGreen']
 RED = _constants['LEDRed']
 OFF = _constants['LEDOff']
+OK = _constants['StatusOK']
+ERROR = _constants['StatusError']
+BINNING_OFF = 0
+BINNING_ON = 1
+GAIN1X = _constants['Gain1x']
+GAIN2_5X = _constants['Gain2_5x']
+GAIN4X = _constants['Gain4x']
+GAIN5X = _constants['Gain5x']
+ALL_ROWS = _constants['RowsDefault']
+
+# Define user-friendly dicts to look up names from values in context.
+_status_constants   = [ OK,   ERROR ]
+_status_names       = ['OK', 'ERROR']
+status_dict = dict(zip(_status_constants, _status_names))
+_led_setting_constants  = [ OFF,   GREEN,   RED ]
+_led_setting_names      = ['OFF', 'GREEN', 'RED']
+led_dict = dict(zip(_led_setting_constants, _led_setting_names))
+_binning_constants  = [ BINNING_OFF,   BINNING_ON ]
+_binning_names      = ['BINNING_OFF', 'BINNING_ON']
+binning_dict = dict(zip(_binning_constants, _binning_names))
+_gain_constants  = [ GAIN1X,   GAIN2_5X,   GAIN4X,   GAIN5X ]
+_gain_names      = ['GAIN1X', 'GAIN2_5X', 'GAIN4X', 'GAIN5X']
+gain_dict = dict(zip(_gain_constants, _gain_names))
+rows_dict = {ALL_ROWS: 'ALL_ROWS'}
 
