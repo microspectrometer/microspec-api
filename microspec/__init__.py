9cc01eb564707bfe77119e63f65c7fbe55312b7d
sustainablelab <sustainablelab@gmail.com>
2020-08-31 17:02:42 -0400

    Improve documentation of modules in __init__.py

--- microspec/__init__.py
+++ microspec/__init__.py
@@ -32,49 +32,93 @@ microspec.Devkit()``, Chromation recommends importing
 Module List
 -----------
 
-The ``microspec`` package has three modules:
-:mod:`~microspec.commands`, :mod:`~microspec.constants`, and
-:mod:`~microspec.replies`
+The ``microspec`` package has four modules:
+:mod:`~microspec.commands`, :mod:`~microspec.constants`,
+:mod:`~microspec.replies`, and :mod:`~microspec.helpers`.
 
 :mod:`microspec.commands`
 ^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-- defines class :class:`~microspec.commands.Devkit`
-- each command is a method in the ``Devkit`` class
-- Example:
+:mod:`~microspec.commands` defines **class**
+:class:`~microspec.commands.Devkit`:
+
+.. code-block:: python
+   :emphasize-lines: 2
+
+   import microspec
+   kit = microspec.Devkit()
+
+Each command in the serial communication protocol is a ``Devkit``
+**method**.
+
+Example:
 
 .. code-block:: python
 
-    import microspec
-    kit = microspec.Devkit()
-    kit.getBridgeLED()
+   kit.getBridgeLED()
+
+.. note::
+
+   The dev-kit commands are defined in the ``protocol.command``
+   object of the :ref:`JSON API config file <dev-kit-API-JSON>`.
+   The keys are the byte values sent over serial to represent the
+   command. The values are objects that name the command and
+   describe its parameters and byte format. These names are
+   identical to the method names of ``Devkit`` except that the
+   first letter is capitalized.
 
 :mod:`microspec.constants`
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-- defines names for constants
-- Example:
+:mod:`~microspec.constants` **names** the constants in the serial
+communication protocol.
 
-.. code-block:: python
+  Example:
 
-    import microspec as usp
-    usp.OK      # equals int 0
-    usp.ERROR   # equals int 1
+  .. code-block:: python
+     :emphasize-lines: 2-3
 
-- defines dictionaries for converting an integer code to its
-  constant's name
-- Example:
+     import microspec as usp
+     usp.OK    # <---------------------- equals int 0
+     usp.ERROR # <---------------------- equals int 1
 
-.. code-block:: python
+:mod:`~microspec.constants` provides **dictionaries** for
+converting a constant's **integer code** into a **string name**
 
-    import microspec as usp
-    usp.status_dict.get(usp.OK) # returns str 'OK'
+  Example:
 
-- values of the constants match the ``globals`` section of the
-  :ref:`JSON API config file <dev-kit-API-JSON>`
+  .. code-block:: python
+     :emphasize-lines: 2
+
+     import microspec as usp
+     usp.status_dict.get(usp.OK) # <---- returns str 'OK'
+
+  .. note::
+
+     Applications usually do not use these dictionaries. Module
+     :mod:`~microspec.replies` uses these dictionaries to make
+     command responses human-readable.
+
+  .. note::
+
+     Values of the constants match the values in the ``globals``
+     object in the :ref:`JSON API config file
+     <dev-kit-API-JSON>`. Names of the constants are similar to
+     the keys in the ``globals`` object, but the names are
+     capitalized. Where possible, names are shortened for
+     readability.
+
+     :mod:`~microspec.constants` defines additional constants
+     ``MAX_CYCLES`` and ``MIN_CYCLES``. These are hard-coded in
+     the dev-kit firmware but are not (yet) listed in the JSON
+     config file.
 
 :mod:`microspec.replies`
 ^^^^^^^^^^^^^^^^^^^^^^^^^
+
+:mod:`~microspec.replies` defines a ``namedtuple`` for the
+response to each command. Class ``Devkit`` uses ``replies`` to
+re-package the response returned by the ``microspeclib`` API.
 
 .. note::
 
@@ -85,9 +129,33 @@ The ``microspec`` package has three modules:
     :mod:`~microspec.replies` to format responses. Application
     code should never need to instantiate a response.
 
-- defines a namedtuple for the response to each command
-- class ``Devkit`` uses ``replies`` to re-package the
-  response returned by the ``microspeclib`` API
+.. note::
+
+   The responses to each command are defined in the
+   ``protocol.sensor`` object in the :ref:`JSON API config file
+   <dev-kit-API-JSON>`. The responses to Bridge-specific commands
+   are in ``protocol.bridge``. The API hides the bridge responses
+   to commands directed at the sensor, which is most commands.
+
+:mod:`microspec.helpers`
+^^^^^^^^^^^^^^^^^^^^^^^^
+
+:mod:`~microspec.helpers` provides helper functions for common
+application tasks. Use the helpers to reduce lines of code.
+
+Example:
+
+.. code-block:: python
+   :emphasize-lines: 2
+
+   import microspec as usp
+   ms = usp.to_ms(usp.MAX_CYCLES)
+
+The :mod:`~microspec.commands` module incorporates helpers where
+possible. For example, :func:`~microspec.commands.setExposure`
+and :func:`~microspec.commands.getExposure` already incorporate
+the time conversion helpers.
+
 """
 from .commands import * # class Devkit
 from .constants import * # OK, ERROR, OFF, GREEN, RED, etc.
